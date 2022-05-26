99b22f922092e82cd4bc3414a10fd8fa7a607aa5
zezerinodev <mattoszz@hotmail.com>
2022-05-26 19:09:29 -0300

    Foi criado o Methodo Delete

--- backend/src/routes/TaskRoutes.js
+++ backend/src/routes/TaskRoutes.js
@@ -9,6 +9,8 @@ const MacaddressValidation = require('../middlewares/MacaddressValidation');
 router.post('/', TaskValidation, TaskController.create);
 router.put('/:id', TaskValidation,TaskController.update);
 router.get('/:id', TaskController.show);
+router.delete('/:id', TaskController.delete);
+
 router.get('/filter/all', MacaddressValidation, TaskController.all);
 
 module.exports = router;
