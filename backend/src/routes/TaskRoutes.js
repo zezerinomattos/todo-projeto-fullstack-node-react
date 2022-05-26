8311348b73f95f5d961ef75a3ffc8b9f81e501e4
zezerinodev <mattoszz@hotmail.com>
2022-05-26 18:25:51 -0300

    Criando a função para trazer uma atividade polo id

--- backend/src/routes/TaskRoutes.js
+++ backend/src/routes/TaskRoutes.js
@@ -8,6 +8,7 @@ const MacaddressValidation = require('../middlewares/MacaddressValidation');
 
 router.post('/', TaskValidation, TaskController.create);
 router.put('/:id', TaskValidation,TaskController.update);
+router.get('/:id', TaskController.show);
 router.get('/filter/all', MacaddressValidation, TaskController.all);
 
 module.exports = router;
