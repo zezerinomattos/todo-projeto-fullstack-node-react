9ad56c62cdaaf8eaf4e704d9032a827325315009
zezerinodev <mattoszz@hotmail.com>
2022-05-27 18:24:51 -0300

    Rota para filtrar atividades por semana finalizada

--- backend/src/controller/TaskController.js
+++ backend/src/controller/TaskController.js
@@ -1,6 +1,6 @@
 const response = require('express');
 const TaskModel = require('../model/TaskModel');
-const { startOfDay, endOfDay} = require('date-fns');
+const { startOfDay, endOfDay, startOfWeek, endOfWeek } = require('date-fns');
 
 const current = new Date();
 
@@ -94,6 +94,21 @@ class TaskController {
         await TaskModel.find({
             'macaddress': {'$in': req.body.macaddress},
             'when': {'$gte': startOfDay(current), '$lte': endOfDay(current)}
+        })
+            .sort('when')
+            .then(response => {
+                return res.status(200).json(response);
+            })
+            .catch(error => {
+                return res.status(500).json(error);
+            });
+    }
+
+    async week(req, res){
+        //$gte = maior ou igual a,  $lt = menor ou igual que
+        await TaskModel.find({
+            'macaddress': {'$in': req.body.macaddress},
+            'when': {'$gte': startOfWeek(current), '$lte': endOfWeek(current)}
         })
             .sort('when')
             .then(response => {
--- backend/src/routes/TaskRoutes.js
+++ backend/src/routes/TaskRoutes.js
@@ -15,5 +15,6 @@ router.put('/:id/:done', TaskController.done);
 router.get('/filter/all', MacaddressValidation, TaskController.all);
 router.get('/filter/late', MacaddressValidation, TaskController.late);
 router.get('/filter/today', MacaddressValidation, TaskController.today);
+router.get('/filter/week', MacaddressValidation, TaskController.week);
 
 module.exports = router;
