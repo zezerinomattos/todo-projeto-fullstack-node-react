e95324132e2158c563f63bc341cc4d9515de2302
zezerinodev <mattoszz@hotmail.com>
2022-06-24 21:04:19 -0300

    Componente Footer finalizado

---
+++ frontend/web/src/components/Footer/index.js
@@ -0,0 +1,27 @@
+
+/*
+import React from 'react';
+import './styles.css';
+
+
+function Footer() {
+  return (
+    <span id='container'>ToDO - Organizando sua vida</span>
+  )
+}
+
+export default Footer;
+*/
+
+import React from 'react';
+import * as S from './styles';
+
+function Footer() {
+  return (
+    <S.Container>
+        <span >ToDO - Organizando sua vida</span>
+    </S.Container>
+  )
+}
+
+export default Footer;
--- frontend/web/src/views/Home/index.js
+++ frontend/web/src/views/Home/index.js
@@ -3,11 +3,13 @@ import * as S from './styles';
 
 //Nossos Componentes
 import Header from '../../components/Header';
+import Footer from '../../components/Footer';
 
 function Home() {
   return (
     <S.Container>
       <Header/>
+      <Footer/>
     </S.Container>
   )
 }
