ea3bfaf1ba99cc065525ad816c340a1a7bf7af87
zezerinodev <mattoszz@hotmail.com>
2022-11-17 12:15:54 -0300

    Modificando conexão com Mongo

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
 
-const url = 'mongodb://localhost:27017/todo';
+// const url = 'mongodb://localhost:27017/todo';
+const url = 'mongodb://127.0.0.1:27017/todo'
 
 // para ele ter compatibilidade com outras versões de mongo
 mongoose.connect(url, {useNewUrlParser: true}); 
 
 //Para poder esse arquivo em outros lugares
 module.exports = mongoose;
+
